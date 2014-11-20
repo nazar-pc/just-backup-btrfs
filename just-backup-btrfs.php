4b845d8e55b2c8e76888d43c390cf85a10b272aa
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-11-20 10:07:06 +0100

    Initial commit

---
+++ just-backup-btrfs.php
@@ -0,0 +1,178 @@
+#!/usr/bin/php
+<?php
+/**
+ * @package		Just backup btrfs
+ * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright	Copyright (c) 2014, Nazar Mokrynskyi
+ * @license		http://opensource.org/licenses/MIT
+ * @version		0.1
+ */
+set_time_limit(0);
+@ini_set('max_input_time', 0);
+
+echo "Just backup btrfs started...\n";
+
+$config	= json_decode(file_get_contents('just-backup-btrfs.json'), true);
+
+/**
+ * @param SQLite3	$history_db
+ * @param int		$keep
+ * @param string	$interval
+ *
+ * @return int
+ */
+function keep_or_not ($history_db, $keep, $interval) {
+	if ($keep == -1) {
+		return 1;
+	}
+	switch ($interval) {
+		case 'hour':
+			$offset		= 3600 / $keep;
+			$condition	= '`keep_year` = 1 OR `keep_month` = 1 OR `keep_day` = 1 OR `keep_hour` = 1';
+		break;
+		case 'day':
+			$offset		= 3600 * 24 / $keep;
+			$condition	= '`keep_year` = 1 OR `keep_month` = 1 OR `keep_day` = 1';
+		break;
+		case 'month':
+			$offset		= 3600 * 24 * 30 / $keep;
+			$condition	= '`keep_year` = 1 OR `keep_month` = 1';
+		break;
+		case 'year':
+			$offset		= 3600 * 24 * 365 / $keep;
+			$condition	= '`keep_year` = 1';
+		break;
+		default:
+			return 1;
+	}
+	return $history_db->querySingle(
+		"SELECT `snapshot_name`
+		FROM `history`
+		WHERE
+			($condition) AND
+			`date`		> ".(time() - $offset)
+	) ? 0 : 1;
+}
+
+foreach ($config as $local_config) {
+	$source			= $local_config['source_mounted_volume'];
+	$destination	= $local_config['destination_within_partition'];
+	$keep_snapshots	= $local_config['keep_snapshots'];
+
+	if (!file_exists($destination)) {
+		if (!mkdir($destination, 0755, true)) {
+			echo "Creating backup destination $destination failed, skip backing up $source, check permissions\n";
+			continue;
+		}
+	}
+
+	if (isset($history_db)) {
+		$history_db->close();
+	}
+	$history_db	= new SQLite3("$destination/history.db");
+	if (!$history_db) {
+		echo "Opening database $destination/history.db failed, skip backing up $source, check permissions\n";
+		continue;
+	}
+	$history_db->exec(
+		"CREATE TABLE IF NOT EXISTS `history` (
+			`snapshot_name` TEXT,
+			`comment` TEXT,
+			`date` INTEGER,
+			`keep_hour` INTEGER,
+			`keep_day` INTEGER,
+			`keep_month` INTEGER,
+			`keep_year` INTEGER
+		)"
+	);
+
+	$keep_year			= keep_or_not($history_db, $keep_snapshots['year'], 'year');
+	$keep_month			= $keep_year	? 1 : keep_or_not($history_db, $keep_snapshots['month'], 'month');
+	$keep_day			= $keep_month	? 1 : keep_or_not($history_db, $keep_snapshots['day'], 'day');
+	$keep_hour			= $keep_day		? 1 : keep_or_not($history_db, $keep_snapshots['hour'], 'hour');
+	if (!$keep_hour) {
+		continue;
+	}
+
+	$date		= time();
+	$snapshot	= date($local_config['date_format'], $date);
+	shell_exec("btrfs subvolume snapshot -r \"$source\" \"$destination/$snapshot\"");
+	if (!file_exists("$destination/$snapshot")) {
+		echo "Snapshot creation for $source failed\n";
+		continue;
+	}
+
+	$snapshot_escaped	= $history_db->escapeString($snapshot);
+	$comment			= ''; //TODO Comments support
+	if (!$history_db->exec(
+		"INSERT INTO `history` (
+			`snapshot_name`,
+			`comment`,
+			`date`,
+			`keep_hour`,
+			`keep_day`,
+			`keep_month`,
+			`keep_year`
+		) VALUES (
+			'$snapshot_escaped',
+			'$comment',
+			'$date',
+			'$keep_hour',
+			'$keep_day',
+			'$keep_month',
+			'$keep_year'
+		)"
+	)) {
+		echo "Snapshot $snapshot for $source created successfully, but not added to history because of database error\n";
+		continue;
+	}
+	echo "Snapshot $snapshot for $source created successfully\n";
+
+	$snapshots_for_removal	= [];
+	$snapshots				= $history_db->query(
+		"SELECT `snapshot_name`
+		FROM `history`
+		WHERE
+			`keep_day`	= 0 AND
+			`date`		< ".(time() - 3600)
+	);
+	while ($snapshots_for_removal[] = $snapshots->fetchArray(SQLITE3_ASSOC)['snapshot_name']);
+	$snapshots				= $history_db->query(
+		"SELECT `snapshot_name`
+		FROM `history`
+		WHERE
+			`keep_month`	= 0 AND
+			`date`			< ".(time() - 3600 * 24)
+	);
+	while ($snapshots_for_removal[] = $snapshots->fetchArray(SQLITE3_ASSOC)['snapshot_name']);
+	$snapshots				= $history_db->query(
+		"SELECT `snapshot_name`
+		FROM `history`
+		WHERE
+			`keep_year`	= 0 AND
+			`date`		< ".(time() - 3600 * 24 * 30)
+	);
+	while ($snapshots_for_removal[] = $snapshots->fetchArray(SQLITE3_ASSOC)['snapshot_name']);
+	$snapshots_for_removal	= array_unique($snapshots_for_removal);
+	$snapshots_for_removal	= array_filter($snapshots_for_removal);
+	foreach ($snapshots_for_removal as $snapshot_for_removal) {
+		shell_exec("btrfs subvolume delete \"$destination/$snapshot_for_removal\"");
+		if (file_exists("$destination/$snapshot_for_removal")) {
+			echo "Removing old snapshot $snapshot_for_removal for $source failed\n";
+			continue;
+		}
+		$snapshot_for_removal_escaped	= $history_db->escapeString($snapshot_for_removal);
+		if (!$history_db->exec(
+			"DELETE FROM `history`
+			WHERE `snapshot_name` = '$snapshot_for_removal_escaped'"
+		)) {
+			echo "Old snapshot $snapshot_for_removal for $source removed successfully, but not removed from history because of database error\n";
+			continue;
+		}
+		echo "Old snapshot $snapshot_for_removal for $source removed successfully\n";
+	}
+}
+if (isset($history_db)) {
+	$history_db->close();
+}
+echo "Just backup btrfs finished!\n";
---
+++ readme.md
@@ -0,0 +1,79 @@
+### Just backup btrfs
+Script that does just that - creates backups using snapshot of btrfs filesystem. Also it makes rotation of snapshots by removing old ones and keeping as many snapshots as you want.
+
+
+### Why?
+I wanted simple thing: store snapshots of several subvolumes in one place and keep specific number of snapshots for each time interval (more recent and less elder snapshots).
+
+I was wondered that no existing solutions fit my needs, so I decided to write it:)
+
+### Requirements
+* php5-cli (version 5.4+)
+* php5-sqlite
+
+You can get them both on Ubuntu:
+```bash
+sudo apt-get install php5-cli php5-sqlite
+```
+
+Also enable sqlite3 extension since it may be disabled by default:
+```bash
+sudo php5enmod sqlite3
+```
+
+### Usage
+Script expects configuration file to be present at location `/etc/just-backup-btrfs.json`, example of such file you can find in this repository.
+
+There are few ways to run script.
+```bash
+sudo php just-backup-btrfs.php
+```
+
+or mark file as executable and just
+```bash
+./just-backup-btrfs.php
+```
+
+or mark as executable, rename it to `just-backup-btrfs` (since file can't contain dots in that place) and put into `/etc/cron.daily` to make backups every day.
+
+Also you can call it with cron or in some other way:)
+
+### What it actually does?
+* reads configuration from `/etc/just-backup-btrfs.json`
+* creates `destination/history.db` SQLite database if it doesn't exists yet
+* creates snapshot with date as the name inside `destination`
+* store snapshot name, date and how long snapshot should be kept in `destination/history.db`
+* removes old snapshots stored in `destination/history.db` and remove them from it
+
+### Configuration
+Configuration options are especially made self-explanatory:
+```json
+[
+	{
+		"source_mounted_volume"			: "/",
+		"destination_within_partition"	: "/backup/root",
+		"date_format"					: "Y-m-d_H:i:s",
+		"keep_snapshots"				: {
+			"hour"	: 60,
+			"day"	: 24,
+			"month"	: 30,
+			"year"	: 48
+		}
+	},
+	{
+		"source_mounted_volume"			: "/home",
+		"destination_within_partition"	: "/backup/home",
+		"date_format"					: "Y-m-d_H:i:s",
+		"keep_snapshots"				: {
+			"hour"	: 120,
+			"day"	: 48,
+			"month"	: 60,
+			"year"	: 96
+		}
+	}
+]
+```
+The only thing that may not be obvious here - you can use `-1` as value for `keep_snapshots` elements to allow storing of all created snapshots.
+
+### License
+MIT, feel free to hack it and share!
